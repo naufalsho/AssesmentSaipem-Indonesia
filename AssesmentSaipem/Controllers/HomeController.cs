0aa9c7be6e569ac19ea63e07591c8ce97ef8c06d
naufal.sholahuddin <naufal.sholahuddin@RAS-NAUFAL>
2023-05-24 21:17:00 +0700

    Add project files.

---
+++ AssesmentSaipem/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using AssesmentSaipem.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+namespace AssesmentSaipem.Controllers
+{
+    public class HomeController : Controller
+    {
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+        SqlConnection con = new SqlConnection();
+        List<DataCompany> dataCompanies = new List<DataCompany>();
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            con.ConnectionString = AssesmentSaipem.Properties.Resources.ConnectionString;
+        }
+
+        public IActionResult Index()
+        {
+            FetchData();
+            return View(dataCompanies);
+        }
+
+        private void FetchData()
+        {
+            if(dataCompanies.Count > 0)
+            {
+                dataCompanies.Clear();
+            }
+            try
+            {
+                con.Open();
+                com.Connection = con;
+                com.CommandText = "SELECT [c1Code1], [c1Name], [c2Code2], [c2Name] FROM [DataCompany] WHERE [c1Name] = 'SAIPEM' ORDER BY [c2Code2] DESC;";
+                dr = com.ExecuteReader();
+                while (dr.Read())
+                {
+                    dataCompanies.Add(new DataCompany()
+                    {
+                        c1Code1 = dr["c1Code1"].ToString(),
+                        c1Name = dr["c1Name"].ToString(),
+                        c2Code2 = dr["c2Code2"].ToString(),
+                        c2Name = dr["c2Name"].ToString()
+                    });
+                }
+                con.Close();
+            }
+                catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+                throw ex;
+            }
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AssesmentSaipem/Models/DataCompany.cs
@@ -0,0 +1,10 @@
+﻿namespace AssesmentSaipem.Models
+{
+    public class DataCompany
+    {
+        public string c1Code1 { get; set; }
+        public string c1Name { get; set; }
+        public string c2Code2 { get; set; }
+        public string c2Name { get; set; }
+    }
+}
